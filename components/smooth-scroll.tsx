675ec0901ab545db645514713bdc37d1630d6184
Noé Henchoz <henchoznoe@gmail.com>
2025-11-27 23:13:22 +0100

    feat: Force scroll to top on reload and set scroll restoration to manual.

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -5,6 +5,14 @@ import { useEffect } from 'react'
 
 export const SmoothScroll = () => {
     useEffect(() => {
+        // Force scroll to top on reload
+        if (typeof window !== 'undefined') {
+            window.scrollTo(0, 0)
+            if ('scrollRestoration' in history) {
+                history.scrollRestoration = 'manual'
+            }
+        }
+
         const lenis = new Lenis({
             duration: 1.2,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
