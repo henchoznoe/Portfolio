85f079c31670d6212bbc9421c21ff146847aae03
Noé Henchoz <henchoznoe@gmail.com>
2025-11-28 09:05:23 +0100

    feat: dynamically set Next.js allowed dev origins.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from 'next'
 
+import os from 'os'
+
+const getLocalIp = () => {
+    const interfaces = os.networkInterfaces()
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]!) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address
+            }
+        }
+    }
+    return '127.0.0.1'
+}
+
 const nextConfig: NextConfig = {
-    /* config options here */
+    allowedDevOrigins: [getLocalIp()],
 }
 
 export default nextConfig
